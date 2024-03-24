dd25bead562939d53a240627b77334def8043a33
kyday <kyday63@gmail.com>
2024-03-25 05:17:03 +0900

    fix: prettier-plugin-tailwindcss 수정

--- .pnp.cjs
+++ .pnp.cjs
@@ -41,6 +41,7 @@ const RAW_RUNTIME_STATE =
           ["next", "virtual:a7ee00610a8e7e227045cf043d1629ca8bb479469b79d846783455532afc1de5dbbc5576391a68279f5f1155bae96274a7acaa6b84712a1981699a793bf5b0d8#npm:14.1.4"],\
           ["postcss", "npm:8.4.38"],\
           ["prettier", "npm:3.2.5"],\
+          ["prettier-plugin-tailwindcss", "virtual:a7ee00610a8e7e227045cf043d1629ca8bb479469b79d846783455532afc1de5dbbc5576391a68279f5f1155bae96274a7acaa6b84712a1981699a793bf5b0d8#npm:0.5.12"],\
           ["react", "npm:18.2.0"],\
           ["react-dom", "virtual:a7ee00610a8e7e227045cf043d1629ca8bb479469b79d846783455532afc1de5dbbc5576391a68279f5f1155bae96274a7acaa6b84712a1981699a793bf5b0d8#npm:18.2.0"],\
           ["tailwindcss", "npm:3.4.1"],\
@@ -2641,6 +2642,7 @@ const RAW_RUNTIME_STATE =
           ["next", "virtual:a7ee00610a8e7e227045cf043d1629ca8bb479469b79d846783455532afc1de5dbbc5576391a68279f5f1155bae96274a7acaa6b84712a1981699a793bf5b0d8#npm:14.1.4"],\
           ["postcss", "npm:8.4.38"],\
           ["prettier", "npm:3.2.5"],\
+          ["prettier-plugin-tailwindcss", "virtual:a7ee00610a8e7e227045cf043d1629ca8bb479469b79d846783455532afc1de5dbbc5576391a68279f5f1155bae96274a7acaa6b84712a1981699a793bf5b0d8#npm:0.5.12"],\
           ["react", "npm:18.2.0"],\
           ["react-dom", "virtual:a7ee00610a8e7e227045cf043d1629ca8bb479469b79d846783455532afc1de5dbbc5576391a68279f5f1155bae96274a7acaa6b84712a1981699a793bf5b0d8#npm:18.2.0"],\
           ["tailwindcss", "npm:3.4.1"],\
@@ -4299,6 +4301,88 @@ const RAW_RUNTIME_STATE =
         "packageDependencies": [\
           ["prettier-linter-helpers", "npm:1.0.0"],\
           ["fast-diff", "npm:1.3.0"]\
+        ],\
+        "linkType": "HARD"\
+      }]\
+    ]],\
+    ["prettier-plugin-tailwindcss", [\
+      ["npm:0.5.12", {\
+        "packageLocation": "../../.yarn/berry/cache/prettier-plugin-tailwindcss-npm-0.5.12-8b605c4889-10c0.zip/node_modules/prettier-plugin-tailwindcss/",\
+        "packageDependencies": [\
+          ["prettier-plugin-tailwindcss", "npm:0.5.12"]\
+        ],\
+        "linkType": "SOFT"\
+      }],\
+      ["virtual:a7ee00610a8e7e227045cf043d1629ca8bb479469b79d846783455532afc1de5dbbc5576391a68279f5f1155bae96274a7acaa6b84712a1981699a793bf5b0d8#npm:0.5.12", {\
+        "packageLocation": "./.yarn/__virtual__/prettier-plugin-tailwindcss-virtual-f04fef6d52/3/.yarn/berry/cache/prettier-plugin-tailwindcss-npm-0.5.12-8b605c4889-10c0.zip/node_modules/prettier-plugin-tailwindcss/",\
+        "packageDependencies": [\
+          ["prettier-plugin-tailwindcss", "virtual:a7ee00610a8e7e227045cf043d1629ca8bb479469b79d846783455532afc1de5dbbc5576391a68279f5f1155bae96274a7acaa6b84712a1981699a793bf5b0d8#npm:0.5.12"],\
+          ["@ianvs/prettier-plugin-sort-imports", null],\
+          ["@prettier/plugin-pug", null],\
+          ["@shopify/prettier-plugin-liquid", null],\
+          ["@trivago/prettier-plugin-sort-imports", null],\
+          ["@types/ianvs__prettier-plugin-sort-imports", null],\
+          ["@types/prettier", null],\
+          ["@types/prettier-plugin-astro", null],\
+          ["@types/prettier-plugin-css-order", null],\
+          ["@types/prettier-plugin-import-sort", null],\
+          ["@types/prettier-plugin-jsdoc", null],\
+          ["@types/prettier-plugin-marko", null],\
+          ["@types/prettier-plugin-organize-attributes", null],\
+          ["@types/prettier-plugin-organize-imports", null],\
+          ["@types/prettier-plugin-sort-imports", null],\
+          ["@types/prettier-plugin-style-order", null],\
+          ["@types/prettier-plugin-svelte", null],\
+          ["@types/prettier-plugin-twig-melody", null],\
+          ["@types/prettier__plugin-pug", null],\
+          ["@types/shopify__prettier-plugin-liquid", null],\
+          ["@types/trivago__prettier-plugin-sort-imports", null],\
+          ["prettier", "npm:3.2.5"],\
+          ["prettier-plugin-astro", null],\
+          ["prettier-plugin-css-order", null],\
+          ["prettier-plugin-import-sort", null],\
+          ["prettier-plugin-jsdoc", null],\
+          ["prettier-plugin-marko", null],\
+          ["prettier-plugin-organize-attributes", null],\
+          ["prettier-plugin-organize-imports", null],\
+          ["prettier-plugin-sort-imports", null],\
+          ["prettier-plugin-style-order", null],\
+          ["prettier-plugin-svelte", null],\
+          ["prettier-plugin-twig-melody", null]\
+        ],\
+        "packagePeers": [\
+          "@ianvs/prettier-plugin-sort-imports",\
+          "@prettier/plugin-pug",\
+          "@shopify/prettier-plugin-liquid",\
+          "@trivago/prettier-plugin-sort-imports",\
+          "@types/ianvs__prettier-plugin-sort-imports",\
+          "@types/prettier-plugin-astro",\
+          "@types/prettier-plugin-css-order",\
+          "@types/prettier-plugin-import-sort",\
+          "@types/prettier-plugin-jsdoc",\
+          "@types/prettier-plugin-marko",\
+          "@types/prettier-plugin-organize-attributes",\
+          "@types/prettier-plugin-organize-imports",\
+          "@types/prettier-plugin-sort-imports",\
+          "@types/prettier-plugin-style-order",\
+          "@types/prettier-plugin-svelte",\
+          "@types/prettier-plugin-twig-melody",\
+          "@types/prettier",\
+          "@types/prettier__plugin-pug",\
+          "@types/shopify__prettier-plugin-liquid",\
+          "@types/trivago__prettier-plugin-sort-imports",\
+          "prettier-plugin-astro",\
+          "prettier-plugin-css-order",\
+          "prettier-plugin-import-sort",\
+          "prettier-plugin-jsdoc",\
+          "prettier-plugin-marko",\
+          "prettier-plugin-organize-attributes",\
+          "prettier-plugin-organize-imports",\
+          "prettier-plugin-sort-imports",\
+          "prettier-plugin-style-order",\
+          "prettier-plugin-svelte",\
+          "prettier-plugin-twig-melody",\
+          "prettier"\
         ],\
         "linkType": "HARD"\
       }]\
