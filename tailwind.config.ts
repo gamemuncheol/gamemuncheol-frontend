b38d1e0979916e26bfaf84e5cde14f7ef7deff7d
kyday <kyday63@gmail.com>
2024-04-08 13:55:34 +0900

    feat: tailwind theme 적용 완료

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,32 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      mainPurple: '#5E44DD',
+      mainPink: '#E2148E',
+      black: '#222222',
+      black900: '#26282C',
+      black800: '#333436',
+      black700: '#383B40',
+      black600: '#484E59',
+      black500: '#626273',
+      black400: '#8C949E',
+      black300: '#BDC0C8',
+      black200: '#D7D9DA',
+      black100: '#F3F3F3',
+      white: '#FFFFFF',
+    },
+    fontFamily: {
+      sansNeoBold: ['SpoqaHanSansNeo-Bold', 'sans-serif'],
+      sansNeoMedium: ['SpoqaHanSansNeo-Medium', 'sans-serif'],
+      SansNeoRegular: ['SpoqaHanSansNeo-Regular', 'sans-serif'],
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
