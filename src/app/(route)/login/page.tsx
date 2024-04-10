1171f9eb8c8a6fd08db86b1ed2be593a20e00c09
0214kbs <0214kbs@naver.com>
2024-04-11 00:26:20 +0900

    feat: 로그인 페이지 WIP - Login 첫 화면에 필요한 assets 추가 - 기본적인 틀 잡기(로고 위치,구성)

--- src/app/(route)/login/page.tsx
+++ src/app/(route)/login/page.tsx
@@ -1,9 +1,24 @@
-import BackgroundVideo from '@/components/login/background';
-
+import Image from 'next/image';
+import loginlogo from '@/assets/login/loginlogo.svg';
+// import BackgroundVideo from '@/components/login/background';
 export default function Login() {
   return (
     <>
-      <BackgroundVideo />
+      {/* <BackgroundVideo /> */}
+      <div className="flex flex-col items-center justify-center h-screen">
+        <Image
+          width={360}
+          height={200}
+          priority
+          src={loginlogo}
+          placeholder="empty"
+          alt="logo"
+        />
+        <div>정치질과 입롤에 지칠 때는 112말고, 롤문철</div>
+        <div>로그인 후 이용하실 수 있습니다.</div>
+        <div>Apple로 계속하기</div>
+        <div>Google로 계속하기</div>
+      </div>
     </>
   );
 }
