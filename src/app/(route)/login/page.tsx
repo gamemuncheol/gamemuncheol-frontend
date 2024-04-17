47ba439af7ad101e69ba58da98e9a4bd41092407
0214kbs <0214kbs@naver.com>
2024-04-17 13:35:27 +0900

    feat: 로그인 페이지 style 적용

--- src/app/(route)/login/page.tsx
+++ src/app/(route)/login/page.tsx
@@ -2,13 +2,12 @@ import Image from 'next/image';
 import loginlogo from '@/assets/login/loginlogo.svg';
 import applelogo from '@/assets/login/apple.svg';
 import googlelogo from '@/assets/login/google.svg';
-
 // import BackgroundVideo from '@/components/login/background';
 export default function Login() {
   return (
     <>
       {/* <BackgroundVideo /> */}
-      <div className="flex flex-col items-center justify-center h-screen">
+      <div className="flex flex-col items-center justify-center h-screen bg-login-bg gap-6">
         <Image
           width={360}
           height={200}
@@ -17,37 +16,46 @@ export default function Login() {
           placeholder="empty"
           alt="logo"
         />
-        <div className="font-sansNeoMeidum text-[26px]">
-          정치질과 입롤에 지칠 때는
-        </div>
-        <div className="font-sansNeoMeidum text-[26px]"> 112말고, 롤문철</div>
-        <div className="font-sansNeoRegular text=[20px]">
-          로그인 후 이용하실 수 있습니다.
-        </div>
-        <div className="flex justify-center gap-2.5 items-center bg-black800 py-2.5 px-5  w-[358px] h-[50px] rounded-[54px]">
-          <Image
-            width={20}
-            height={20}
-            priority
-            src={applelogo}
-            placeholder="empty"
-            alt="apple"
-          />
-          <div className="font-sansNeoMedium text-[16px] text-white">
-            Apple로 계속하기
+        <div className="flex flex-col items-center gap-2.5">
+          <div className="flex flex-col items-center">
+            <div className="font-sansNeoMeidum text-[26px] text-white">
+              정치질과 입롤에 지칠 때는
+            </div>
+            <div className="font-sansNeoMeidum text-[26px] text-white">
+              112말고, 롤문철
+            </div>
+          </div>
+          <div className="font-sansNeoRegular text=[20px] text-white">
+            로그인 후 이용하실 수 있습니다.
           </div>
         </div>
-        <div className="flex justify-center gap-2.5 items-center bg-white py-2.5 px-5  w-[358px] h-[50px] rounded-[54px]">
-          <Image
-            width={20}
-            height={20}
-            priority
-            src={googlelogo}
-            placeholder="empty"
-            alt="google"
-          />
-          <div className="font-sansNeoMedium text-[16px] text-fontColor02">
-            Google로 계속하기
+
+        <div className="flex flex-col gap-4">
+          <div className="flex justify-center gap-2.5 items-center bg-black800 py-2.5 px-5  w-[358px] h-[50px] rounded-[54px]">
+            <Image
+              width={20}
+              height={20}
+              priority
+              src={applelogo}
+              placeholder="empty"
+              alt="apple"
+            />
+            <div className="font-sansNeoMedium text-[16px] text-white">
+              Apple로 계속하기
+            </div>
+          </div>
+          <div className="flex justify-center gap-2.5 items-center bg-white py-2.5 px-5  w-[358px] h-[50px] rounded-[54px]">
+            <Image
+              width={20}
+              height={20}
+              priority
+              src={googlelogo}
+              placeholder="empty"
+              alt="google"
+            />
+            <div className="font-sansNeoMedium text-[16px] text-fontColor02">
+              Google로 계속하기
+            </div>
           </div>
         </div>
       </div>
