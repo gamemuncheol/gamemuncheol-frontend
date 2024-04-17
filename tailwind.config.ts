997c3e885ca23e6f0fa13182eca9fc9d936bd5f8
0214kbs <0214kbs@naver.com>
2024-04-17 13:34:43 +0900

    feat: 로그인 임시 배경이미지 추가 및 적용

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from 'tailwindcss';
-
 const config: Config = {
   content: [
     './src/**/*',
@@ -39,6 +38,7 @@ const config: Config = {
     },
     extend: {
       backgroundImage: {
+        'login-bg': "url('/login/bgtemp.png')",
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
