81be1d9dcbc3813db28a7ad3b594a6cd1c5ab476
0214kbs <0214kbs@naver.com>
2024-04-18 18:20:49 +0900

    feat: 글씨 스타일 적용

--- src/app/(route)/login/page.tsx
+++ src/app/(route)/login/page.tsx
@@ -18,14 +18,10 @@ export default function Login() {
         />
         <div className="flex flex-col items-center gap-2.5">
           <div className="flex flex-col items-center">
-            <div className="font-sansNeoMeidum text-[26px] text-white">
-              정치질과 입롤에 지칠 때는
-            </div>
-            <div className="font-sansNeoMeidum text-[26px] text-white">
-              112말고, 롤문철
-            </div>
+            <div className="title01 text-white">정치질과 입롤에 지칠 때는</div>
+            <div className="title01 text-white">112말고, 롤문철</div>
           </div>
-          <div className="font-sansNeoRegular text=[20px] text-white">
+          <div className="body02 text-white">
             로그인 후 이용하실 수 있습니다.
           </div>
         </div>
@@ -40,9 +36,7 @@ export default function Login() {
               placeholder="empty"
               alt="apple"
             />
-            <div className="font-sansNeoMedium text-[16px] text-white">
-              Apple로 계속하기
-            </div>
+            <div className="body02 text-white">Apple로 계속하기</div>
           </div>
           <div className="flex justify-center gap-2.5 items-center bg-white py-2.5 px-5  w-[358px] h-[50px] rounded-[54px]">
             <Image
@@ -53,9 +47,7 @@ export default function Login() {
               placeholder="empty"
               alt="google"
             />
-            <div className="font-sansNeoMedium text-[16px] text-fontColor02">
-              Google로 계속하기
-            </div>
+            <div className="body02 text-fontColor02">Google로 계속하기</div>
           </div>
         </div>
       </div>
