f06345484b13d40227e59d7085e8b14946f7c341
kyday <kyday63@gmail.com>
2024-04-21 02:43:46 +0900

    feat: Add graph component

--- src/app/(route)/main/layout.tsx
+++ src/app/(route)/main/layout.tsx
@@ -1,21 +1,30 @@
+import { ReactElement } from 'react';
+
 export default function VideoContainerLayout(props: {
   category: string;
   title: string;
   children: React.ReactNode;
+  image: ReactElement;
 }) {
-  const { category, title, children } = props;
+  const { category, title, image, children } = props;
 
   return (
-    <section>
-      <p className="text-sm font-SansNeoRegular text-primary-font-color">
-        {category}
-      </p>
+    <article className="mb-12">
+      <div className="flex items-end">
+        <div>
+          <p className="text-sm font-SansNeoRegular text-primary-font-color">
+            {category}
+          </p>
 
-      <h1 className="whitespace-pre-line text-black900 font-sansNeoBold">
-        {title}
-      </h1>
+          <h1 className="whitespace-pre-line text-black900 font-sansNeoBold">
+            {title}
+          </h1>
+        </div>
+
+        {image}
+      </div>
 
       {children}
-    </section>
+    </article>
   );
 }
