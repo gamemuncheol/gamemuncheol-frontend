d17e9009ae69e355f3df589c310f99e9c716d560
0214kbs <0214kbs@naver.com>
2024-04-30 18:00:16 +0900

    fix: LayoutProvider의 main flex를 제거하여 로그인 페이지의 레이아웃 변동 문제 해결

--- src/layout/LayoutProvider.tsx
+++ src/layout/LayoutProvider.tsx
@@ -21,7 +21,7 @@ export const LayoutProvider = ({
     <>
       {!CommonHideComponent() && <Header />}
 
-      <main className="flex">
+      <main>
         {!CommonHideComponent() && <Navbar />}
         {children}
       </main>
