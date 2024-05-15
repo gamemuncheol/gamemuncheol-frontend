0cfa1e6e387444080bd0796d8586e0769c5aef83
0214kbs <0214kbs@naver.com>
2024-05-15 16:25:38 +0900

    feat: 다시 임시 배경 이미지로 설정 - mux를 통한 영상 : 유료, 크기, 자동재생 등의 문제가 있음 - 임시 이미지로 배경 설정

--- src/app/(route)/login/page.tsx
+++ src/app/(route)/login/page.tsx
@@ -2,46 +2,45 @@ import Image from 'next/image';
 import loginlogo from '@/assets/login/loginlogo.svg';
 import applelogo from '@/assets/login/apple.svg';
 import GoogleLogin from '@/components/Login/googlelogin/Googlelogin';
-// import BackgroundVideo from '@/components/login/background';
-
+import Video from 'next-video';
+// import BackgroundVideo from 'next-video/background-video';
+import bgvideo from '/videos/loginbg.mp4?thumbnailTime=0';
 export default function Login() {
   return (
     <>
-      {/* <BackgroundVideo /> */}
-      <div className="flex flex-col items-center justify-center h-screen bg-login-bg gap-6">
-        <Image
-          width={360}
-          height={200}
-          priority
-          src={loginlogo}
-          placeholder="empty"
-          alt="logo"
-        />
-        <div className="flex flex-col items-center gap-2.5">
-          <div className="flex flex-col items-center">
-            <div className="title01 text-white">정치질과 입롤에 지칠 때는</div>
-            <div className="title01 text-white">112말고, 롤문철</div>
-          </div>
-          <div className="body02 text-white">
-            로그인 후 이용하실 수 있습니다.
-          </div>
+      <Video src={bgvideo} />
+      {/* <div className="flex flex-col items-center justify-center h-screen bg-login-bg gap-6"> */}
+      <Image
+        width={360}
+        height={200}
+        priority
+        src={loginlogo}
+        placeholder="empty"
+        alt="logo"
+      />
+      <div className="flex flex-col items-center gap-2.5">
+        <div className="flex flex-col items-center">
+          <div className="title01 text-white">정치질과 입롤에 지칠 때는</div>
+          <div className="title01 text-white">112말고, 롤문철</div>
         </div>
-
-        <div className="flex flex-col gap-4">
-          <div className="flex justify-center gap-2.5 items-center bg-black800 py-2.5 px-5  w-[358px] h-[50px] rounded-[54px]">
-            <Image
-              width={20}
-              height={20}
-              priority
-              src={applelogo}
-              placeholder="empty"
-              alt="apple"
-            />
-            <div className="body02 text-white">Apple로 계속하기</div>
-          </div>
-          <GoogleLogin />
-        </div>
+        <div className="body02 text-white">로그인 후 이용하실 수 있습니다.</div>
       </div>
+
+      <div className="flex flex-col gap-4">
+        <div className="flex justify-center gap-2.5 items-center bg-black800 py-2.5 px-5  w-[358px] h-[50px] rounded-[54px]">
+          <Image
+            width={20}
+            height={20}
+            priority
+            src={applelogo}
+            placeholder="empty"
+            alt="apple"
+          />
+          <div className="body02 text-white">Apple로 계속하기</div>
+        </div>
+        <GoogleLogin />
+      </div>
+      {/* </div> */}
     </>
   );
 }
