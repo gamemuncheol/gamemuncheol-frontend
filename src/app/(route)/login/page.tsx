8e2264573c6b30a051bedc66d16b1db63520bb92
0214kbs <0214kbs@naver.com>
2024-05-17 14:09:01 +0900

    feat: 로그인 페이지에 Footer 컴포넌트 추가 - Footer 컴포넌트 동적 색상 변경 가능하도록 수정 - 페이지 별 글씨 색상을 props로 조정할 수 있도록 함 - 관련 TypeScript 타입 정의 추가

--- src/app/(route)/login/page.tsx
+++ src/app/(route)/login/page.tsx
@@ -1,12 +1,18 @@
+import Footer from '@/components/@common/footer/Footer';
 import LoginView from '@/components/login/loginview/LoginView';
 // import Video from 'next-video';
 // import bgvideo from '/videos/loginbg.mp4?thumbnailTime=0';
 
 export default function Login() {
   return (
-    <>
+    <div className="h-screen bg-login-bg flex flex-col">
       {/* <Video src={bgvideo} /> */}
-      <LoginView />
-    </>
+      <div className="flex flex-1 flex-col justify-center">
+        <LoginView />
+      </div>
+      <div className="sticky bottom-0">
+        <Footer use="login" />
+      </div>
+    </div>
   );
 }
---
+++ src/types/common-type.ts
@@ -0,0 +1,4 @@
+// @common 폴더 안에 쓰이는 type 정의
+export interface FooterProps {
+  use?: string;
+}
