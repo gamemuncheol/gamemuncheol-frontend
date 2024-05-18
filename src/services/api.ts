2ffbeb06d446576c7cfe8491ba882e03ffc3de1e
0214kbs <0214kbs@naver.com>
2024-05-18 13:37:39 +0900

    feat: 응답 인터셉터 구현

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,7 +5,6 @@ const api = axios.create({
 });
 
 const getToken = () => localStorage.getItem('accessToken');
-const setToken = (token: string) => localStorage.setItem('accessToken', token);
 const getRefreshToken = () => localStorage.getItem('refreshToken');
 
 // 요청 interceptor
@@ -16,5 +15,39 @@ api.interceptors.request.use((config) => {
   }
   return config;
 });
+
+// 응답 interceptor
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const statusCode = error.response?.status;
+    const originalRequest = error.config; // 원래요청
+    if (statusCode === 401 && !originalRequest._retry) {
+      try {
+        // refresh token으로 new access token 요청하기
+        const rToken = getRefreshToken();
+        const res = await axios.post(
+          `${process.env.NEXT_PUBLIC_API_URL}/open-api/member/renew`,
+          {
+            refreshToken: rToken,
+          },
+        );
+        const { accessToken, refreshToken } = res.data.data;
+        localStorage.setItem('accessToken', accessToken);
+        localStorage.setItem('refreshToken', refreshToken);
+
+        // 원래 요청 다시 시도
+        originalRequest.headers['Authorization'] = `Bearer ${accessToken}`;
+        return api(originalRequest); // 원래 요청 다시 시도
+      } catch (refreshError) {
+        console.error('Unable to refresh token:', refreshError);
+        return Promise.reject(refreshError);
+      }
+    }
+    return Promise.reject(error);
+  },
+);
 
 export default api;
