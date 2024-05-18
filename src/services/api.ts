f5f4b5bd469940e8efad6b4614e34a49fd2f3c06
0214kbs <0214kbs@naver.com>
2024-05-18 13:17:13 +0900

    feat: 요청 인터셉터 추가

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,4 +4,17 @@ const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
+const getToken = () => localStorage.getItem('accessToken');
+const setToken = (token: string) => localStorage.setItem('accessToken', token);
+const getRefreshToken = () => localStorage.getItem('refreshToken');
+
+// 요청 interceptor
+api.interceptors.request.use((config) => {
+  const token = getToken();
+  if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`;
+  }
+  return config;
+});
+
 export default api;
--- src/services/useMember.ts
+++ src/services/useMember.ts
@@ -6,9 +6,8 @@ const isAgreedAPI = async () => {
   const accessToken = localStorage.getItem('accessToken');
 
   try {
-    const response = await api.get(`/privacy/is-agreed`, {
-      headers: { Authorization: `Bearer ${accessToken}` },
-    });
+    const response = await api.get(`/privacy/is-agreed`);
+    console.log('API', response.data);
     return response.data.data;
   } catch (error) {
     console.error(error);
