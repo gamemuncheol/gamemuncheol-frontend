46ad9fd4ab1155784c91a1dce364e0c8f7b1ba8c
0214kbs <0214kbs@naver.com>
2024-05-20 14:51:19 +0900

    style: 스타일 정정( 폰트 크기, 색상 정의 부분)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -28,13 +28,12 @@ const config: Config = {
       black200: '#D7D9DA',
       black100: '#F3F3F3',
       white: '#FFFFFF',
+      fontcolor01: '#222222',
+      fontcolor02: '#404040',
+      fontcolor03: '#575757',
       'primary-font-color': '#575757',
     },
-    fontColors: {
-      fontColor01: '#222222',
-      fontColor02: '#404040',
-      fontColor03: '#575757',
-    },
+
     fontFamily: {
       sansNeoBold: ['SpoqaHanSansNeo-Bold', 'sans-serif'],
       sansNeoMedium: ['SpoqaHanSansNeo-Medium', 'sans-serif'],
