0dd4e29660109c602ab216206cfae5c9c972324d
0214kbs <0214kbs@naver.com>
2024-05-31 12:50:45 +0900

    feat: login, signup 페이지 분리 - 토큰이 있으면 /login에 접근 제한되기 때문에,이후 작업은 signup으로 분리

--- src/components/login/loginview/LoginView.tsx
+++ src/components/login/loginview/LoginView.tsx
@@ -3,12 +3,12 @@
 import Image from 'next/image';
 import loginlogo from '@/assets/login/loginlogo.svg';
 import { useSearchParams, useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 import useLoginStore from '@/store/useMemberStore';
 import { GoogleLogin, AppleLogin, UserAgree, Nickname } from '@/components/index';
-import { usePatchAgree } from '@/services/mutations/member';
 import { useIsAgree } from '@/services/queries/member';
+import { useCookies } from 'react-cookie';
 
 export default function LoginView() {
   const router = useRouter();
@@ -16,73 +16,51 @@ export default function LoginView() {
 
   const accessToken = params.get('accessToken') || '';
   const refreshToken = params.get('refreshToken');
+  const [cookies, setCookie, removeCookie] = useCookies(['refreshToken']);
 
   //로그인 성공 여부
   const isLoggined = useLoginStore((state) => state.isLoggined);
   const setIsLoggined = useLoginStore((state) => state.setIsLoggined);
 
-  // modal
-  const [showUserAgree, setShowUserAgree] = useState(false);
-  const [showNickname, setShowNickname] = useState(false);
-
-  useEffect(() => {
-    if (accessToken && refreshToken) {
-      localStorage.setItem('accessToken', accessToken);
-      localStorage.setItem('refreshToken', refreshToken);
-      setIsLoggined(true);
-      router.push('/login');
-    }
-  }, [params]);
-
-  useEffect(() => {
-    const token = localStorage.getItem('refreshToken');
-    if (token) {
-      setIsLoggined(true);
-      setShowUserAgree(true);
-    }
-  }, []);
-
   // is-agreed api
-  const { data: isAgreed, isLoading: isAgreedLoading } = useIsAgree(isLoggined);
-  const { mutate: patchAgree } = usePatchAgree();
-
-  const handleAgreeConfirm = () => {
-    patchAgree();
-    setShowUserAgree(false);
-  };
+  const { data: isAgreed, isLoading: isAgreedLoading } = useIsAgree(accessToken ? true : false);
 
   useEffect(() => {
-    if (isAgreed) {
-      setShowUserAgree(false);
-      setShowNickname(true);
+    if (!isAgreed) {
+      router.push('/signup');
+    } else {
+      router.push('/main');
     }
   }, [isAgreed]);
+
+  // 로그인 성공 시
+  useEffect(() => {
+    if (accessToken && refreshToken) {
+      setCookie('refreshToken', refreshToken);
+      localStorage.setItem('accessToken', accessToken);
+      setIsLoggined(true);
+    }
+  }, [params]);
 
   if (isAgreedLoading) {
     return <div className="body01R text-white">로딩 중.. 잠시만 기다려주세요</div>;
   }
 
   return (
-    <>
-      {!isLoggined && (
-        <div className="flex flex-col items-center justify-center gap-6">
-          <Image width={360} height={200} priority src={loginlogo} placeholder="empty" alt="logo" />
-          <div className="flex flex-col items-center gap-2.5">
-            <div className="flex flex-col items-center">
-              <div className="title01M text-white">정치질과 입롤에 지칠 때는</div>
-              <div className="title01M text-white">112말고, 롤문철</div>
-            </div>
-            <div className="body02R text-white">로그인 후 이용하실 수 있습니다.</div>
-          </div>
-
-          <div className="flex flex-col gap-4">
-            <AppleLogin />
-            <GoogleLogin />
-          </div>
+    <div className="flex flex-col items-center justify-center gap-6">
+      <Image width={360} height={200} priority src={loginlogo} placeholder="empty" alt="logo" />
+      <div className="flex flex-col items-center gap-2.5">
+        <div className="flex flex-col items-center">
+          <div className="title01M text-white">정치질과 입롤에 지칠 때는</div>
+          <div className="title01M text-white">112말고, 롤문철</div>
         </div>
-      )}
-      {showUserAgree && <UserAgree handleAllAgreeConfirm={handleAgreeConfirm} />}
-      {showNickname && <Nickname />}
-    </>
+        <div className="body02R text-white">로그인 후 이용하실 수 있습니다.</div>
+      </div>
+
+      <div className="flex flex-col gap-4">
+        <AppleLogin />
+        <GoogleLogin />
+      </div>
+    </div>
   );
 }
