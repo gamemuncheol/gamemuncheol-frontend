e1e327b63c1e59aee9f4a52414ae0c6692da7378
0214kbs <0214kbs@naver.com>
2024-06-11 15:33:20 +0900

    feat: 메인페이지에 등록 버튼 임시로 생성

--- src/components/@common/navbar/Navbar.tsx
+++ src/components/@common/navbar/Navbar.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 import Image from 'next/image';
 import { navItem } from './data/navList';
 
 export default function Navbar() {
+  const [registerModal, setRegisterModal] = useState(false)
   return (
     <nav className="sticky left-0 h-screen w-[6.5rem] flex flex-col justify-between">
       <ul className="flex flex-col items-center justify-center">
@@ -20,6 +21,9 @@ export default function Navbar() {
             </li>
           );
         })}
+        {/* 임시 등록 버튼 */}
+        <li onClick={()=>{setRegisterModal(true)}}>등록</li> 
+        
       </ul>
     </nav>
   );
