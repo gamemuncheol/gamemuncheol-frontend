1501705b7e9a6593812dd4fec0678d00217a6dd2
0-Chan <kyc4421194@gmail.com>
2024-06-13 03:01:30 +0900

    feat: Navbar에 텍스트 추가 후 CSS 개선
    - '추가' 와 관련된 아이콘도 추가

--- src/components/@common/navbar/Navbar.tsx
+++ src/components/@common/navbar/Navbar.tsx
@@ -5,11 +5,15 @@ import { navItem } from './data/navList';
 
 export default function Navbar() {
   return (
-    <nav className="sticky left-0 h-screen w-[6.5rem] flex flex-col justify-between">
-      <ul className="flex flex-col items-center justify-center">
+    <nav className="sticky left-0 ml-10 flex h-screen flex-shrink-0 flex-grow-0 basis-10 flex-col justify-between">
+      <ul className="flex flex-col items-center justify-center space-y-5">
+        {/* TODO: 친구 목록 */}
+        <li>
+          <p className="text-xs">친구</p>
+        </li>
         {navItem.map((item) => {
           return (
-            <li key={item.id}>
+            <li key={item.id} className="flex flex-col items-center">
               <Image
                 style={{ width: 22, height: 22 }}
                 priority
@@ -17,6 +21,7 @@ export default function Navbar() {
                 placeholder="empty"
                 alt="menu"
               />
+              <p className="text-xs">{item.text}</p>
             </li>
           );
         })}
@@ -24,3 +29,6 @@ export default function Navbar() {
     </nav>
   );
 }
+
+// 791
+// 1312
--- src/components/@common/navbar/data/navList.ts
+++ src/components/@common/navbar/data/navList.ts
@@ -1,18 +1,27 @@
-import like from '@/assets/menu/navbar/like.svg';
 import lolmuncheol from '@/assets/menu/navbar/lolmuncheol.svg';
+import like from '@/assets/menu/navbar/like.svg';
 import video from '@/assets/menu/navbar/video.svg';
+import add from '@/assets/menu/navbar/add.svg';
 
 export const navItem = [
   {
     id: 0,
-    src: like,
+    src: lolmuncheol,
+    text: '롤문챗',
   },
   {
     id: 1,
-    src: lolmuncheol,
+    src: like,
+    text: '좋아요',
   },
   {
     id: 2,
     src: video,
+    text: '나',
+  },
+  {
+    id: 3,
+    src: add,
+    text: '추가',
   },
 ];
