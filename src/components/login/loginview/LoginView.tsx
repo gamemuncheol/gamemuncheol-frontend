1dc905a75aadaa7179fd94c3a1c2e2614e5c1d35
0214kbs <0214kbs@naver.com>
2024-06-21 12:43:24 +0900

    feat: 개인정보 동의 api 관련 부분 제거

--- src/components/login/loginview/LoginView.tsx
+++ src/components/login/loginview/LoginView.tsx
@@ -6,8 +6,7 @@ import { useSearchParams, useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 import useLoginStore from '@/store/useMemberStore';
-import { GoogleLogin, AppleLogin, UserAgree, Nickname } from '@/components/index';
-import { useIsAgree } from '@/services/queries/member';
+import { GoogleLogin, AppleLogin } from '@/components/index';
 import { useCookies } from 'react-cookie';
 
 export default function LoginView() {
@@ -22,19 +21,6 @@ export default function LoginView() {
   const isLoggined = useLoginStore((state) => state.isLoggined);
   const setIsLoggined = useLoginStore((state) => state.setIsLoggined);
 
-  // is-agreed api
-  const { data: isAgreed, isLoading: isAgreedLoading } = useIsAgree(accessToken ? true : false);
-
-  useEffect(() => {
-    if (isLoggined) {
-      if (!isAgreed) {
-        router.push('/signup');
-      } else {
-        router.push('/main');
-      }
-    }
-  }, [isAgreed]);
-
   // 로그인 성공 시
   useEffect(() => {
     if (accessToken && refreshToken) {
@@ -44,19 +30,24 @@ export default function LoginView() {
     }
   }, [params]);
 
-  if (isAgreedLoading) {
-    return <div className="body01R text-white">로딩 중.. 잠시만 기다려주세요</div>;
-  }
-
   return (
     <div className="flex flex-col items-center justify-center gap-6">
-      <Image width={360} height={200} priority src={loginlogo} placeholder="empty" alt="logo" />
+      <Image
+        width={360}
+        height={200}
+        priority
+        src={loginlogo}
+        placeholder="empty"
+        alt="logo"
+      />
       <div className="flex flex-col items-center gap-2.5">
         <div className="flex flex-col items-center">
           <div className="title01M text-white">정치질과 입롤에 지칠 때는</div>
           <div className="title01M text-white">112말고, 롤문철</div>
         </div>
-        <div className="body02R text-white">로그인 후 이용하실 수 있습니다.</div>
+        <div className="body02R text-white">
+          로그인 후 이용하실 수 있습니다.
+        </div>
       </div>
 
       <div className="flex flex-col gap-4">
