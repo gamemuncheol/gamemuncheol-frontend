15b855018dd6ab71dfe5129c3b2c55fbbf104ca2
0214kbs <0214kbs@naver.com>
2024-09-04 15:56:50 +0900

    feat: 게시물 업로드 모달 구조 설계

--- src/components/main/post-upload/PostUpload.tsx
+++ src/components/main/post-upload/PostUpload.tsx
@@ -1,56 +1,81 @@
 import { Modal } from '@/components/@common/modal/Modal';
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
-
-// () => void
 import { CircleInfo, TempInformation, Path } from '@/assets/index';
-const PostUpload = ({ closeModal }: any) => {
-  const tempClick = () => {
-    console.log('click');
+import { modaltitles } from '@/constants/PostUpload';
+
+// 공통
+const PostUpload = ({ closeModal }: { closeModal: () => void }) => {
+  const [currentStep, setCurrentStep] = useState(1);
+
+  const nextStep = () => setCurrentStep(currentStep + 1);
+  const prevStep = () => {
+    if (currentStep === 1) {
+      closeModal();
+    } else {
+      setCurrentStep(currentStep - 1);
+    }
   };
+
   return (
     <div>
       <Modal width="700px">
         <Modal.Header
-          title="전적입력을 위해 게임ID를 입력해 주세요."
+          title={modaltitles[currentStep]}
           subtitle="롤문철이 명쾌하게 판단해드려요."
-          onClose={tempClick}
+          onClose={closeModal}
         />
-
-        <div className="flex flex-col items-center p-5">
-          <div className="flex flex-col gap-4">
-            <div>searchbar - 이후 추가</div>
-            <div>
-              <div className="flex flex-row items-center gap-1">
-                <Image
-                  width={24}
-                  height={24}
-                  priority
-                  src={CircleInfo}
-                  alt="info"
-                />
-                <div className="body05M text-mainPurple">
-                  게임ID는 여기서 찾으실 수 있어요.
-                </div>
-              </div>
-              <Image width={230} height={15} priority src={Path} alt="path" />
-            </div>
-            <Image
-              width={360}
-              height={210}
-              priority
-              src={TempInformation}
-              alt="temp_information"
-            />
-          </div>
-        </div>
+        {currentStep === 1 && <Step1></Step1>}
+        {currentStep === 2 && <Step2></Step2>}
+        {currentStep === 3 && <Step3></Step3>}
         <Modal.Footer
-          leftButton={{ text: '취소', onClick: closeModal }}
-          rightButton={{ text: '다음', onClick: tempClick }}
+          leftButton={{
+            text: currentStep === 1 ? '취소' : '이전',
+            onClick: prevStep,
+          }}
+          rightButton={{ text: '다음', onClick: nextStep }}
         />
       </Modal>
     </div>
   );
 };
 
+const Step1 = () => {
+  return (
+    <div className="flex flex-col items-center p-5">
+      <div className="flex flex-col gap-4">
+        <div>searchbar - 이후 추가</div>
+        <div>
+          <div className="flex flex-row items-center gap-1">
+            <Image
+              width={24}
+              height={24}
+              priority
+              src={CircleInfo}
+              alt="info"
+            />
+            <div className="body05M text-mainPurple">
+              게임ID는 여기서 찾으실 수 있어요.
+            </div>
+          </div>
+          <Image width={230} height={15} priority src={Path} alt="path" />
+        </div>
+        <Image
+          width={360}
+          height={210}
+          priority
+          src={TempInformation}
+          alt="temp_information"
+        />
+      </div>
+    </div>
+  );
+};
+
+const Step2 = () => {
+  return <div> step2 </div>;
+};
+const Step3 = () => {
+  return <div> step3 </div>;
+};
 export default PostUpload;
